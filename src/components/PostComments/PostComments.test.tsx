f778dca763a8a02d1ecd1bcf6775b5d655a145b0
Lucas Santana <santanafx@hotmail.com>
2023-08-15 13:00:59 -0300

    exercicio finalizado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Post from '.'
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+
+  test('Deve adicionar comentário na lista', () => {
+    render(<PostComment />)
+    fireEvent.change(screen.getByTestId('campo-comentario'), {
+      target: {
+        value: 'comentário adicionado 1'
+      }
+    })
+    fireEvent.click(screen.getByTestId('botao-comentario'))
+
+    fireEvent.change(screen.getByTestId('campo-comentario'), {
+      target: {
+        value: 'comentário adicionado 2'
+      }
+    })
+    fireEvent.click(screen.getByTestId('botao-comentario'))
+
+    expect(screen.getByText('comentário adicionado 1')).toBeInTheDocument()
+    expect(screen.getByText('comentário adicionado 2')).toBeInTheDocument()
+  })
+})
